a8438c54137443ffb9b81b29a9ceca1f093d411b
chic-creator-9382 <danessypinedav@gmail.com>
2020-08-15 20:28:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["O", "X", " ", "O", "X", " ", "X", "O", " "]
+board = ["O", "X", " ", "0", "X", " ", "X", "0", " "]
 def display_board(board)
 
 
